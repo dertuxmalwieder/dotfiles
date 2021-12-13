2efc4b5825858898456c81a90143f469d1890b93
Cthulhux <>
2021-12-13 18:19:49 +0000

    New dotfiles: PowerShell.
    FossilOrigin-Name: a5bd8a1970c0c2e67ecf7de083061c3a68f6b0e061e66d4c5e4cd3d63feb7c63

---
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,28 @@
+﻿# Improving PowerShell on Windows Terminal.
+# ------------------------------------------
+
+# ReadLine -> Emacs:
+Import-Module PSReadLine
+Set-PSReadlineOption -EditMode Emacs
+
+# Chocolatey:
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
+Import-Module posh-git
+
+# Simulate the "su" command:
+function su {
+	If (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator))
+	{
+		Start-Process wt -Verb RunAs
+		# Optionally close the non-elevated Windows Terminal:
+		# exit
+	}
+}
+
+# tail-f:
+function tail-f([string]$filename) {
+	Get-Content "$filename" -Tail 10 -Wait
+}
---
+++ powershell/README.md
@@ -0,0 +1,6 @@
+# How to find the PowerShell configuration file
+
+1. Open PowerShell.
+2. Type `notepad $profile`.
+
+There it is.
