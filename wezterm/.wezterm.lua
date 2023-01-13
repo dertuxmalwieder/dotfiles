58169edf0df8497cbc0b52022ac127373db6ba20
Cthulhux <>
2023-01-13 10:53:48 +0000

    WezTerm: Color scheme fix
    FossilOrigin-Name: 4cb0e31a6effcc5fe8f74e93ff344d9ea9042b05aa06387a59ef9e0e9a1e1713

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,7 +1,7 @@
 local wezterm = require 'wezterm'
 return {
 	default_prog = { 'C:\\Program Files\\PowerShell\\7\\pwsh.exe', '--NoLogo' },
-	color_scheme = "Adventure",
+	color_scheme = "Ashes (base16)",
 	font = wezterm.font_with_fallback {
 		'Consolas',
 		'JetBrains Mono',
