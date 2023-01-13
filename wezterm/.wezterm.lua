bba607981dd0f6257d87d91bdeb95effc7b086da
Cthulhux <>
2023-01-13 10:45:25 +0000

    new dotfiles: WezTerm (Windows)
    FossilOrigin-Name: 4333999b0f8681250cbb0f66f186996cb8f51357e97052fb4e1d80a166375f16

---
+++ wezterm/.wezterm.lua
@@ -0,0 +1,11 @@
+local wezterm = require 'wezterm'
+return {
+	default_prog = { 'C:\\Program Files\\PowerShell\\7\\pwsh.exe', '--NoLogo' },
+	color_scheme = "Adventure",
+	font = wezterm.font_with_fallback {
+		'Consolas',
+		'JetBrains Mono',
+		'Fira Code',
+	},
+	font_size = 11.0,
+}
