a2e77be4fca2e0df5ac0ffba08ab27152f001c0f
Cthulhux <>
2023-03-22 11:01:15 +0000

    Powershell: su gone, I use gsudo now
    FossilOrigin-Name: b71a3c00667f79c769d95d5aed61752c939a47e6096a506a17120b4b96695378

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -17,24 +17,6 @@ Invoke-Expression (&sfsu hook)
 # posh-git:
 Import-Module posh-git
 
-# Simulate the "su" command:
-function su {
-	If (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator))
-	{
-		If ($env:ALACRITTY_LOG -ne $null) {
-			# Alacritty:
-			Start-Process alacritty -Verb RunAs
-		}
-		Else {
-			# Probably, Windows Terminal (at least on my computers):
-			Start-Process wt -Verb RunAs
-		}
-
-		# Optionally close the non-elevated terminal:
-		# exit
-	}
-}
-
 # tail-f:
 function tail-f([string]$filename) {
 	Get-Content "$filename" -Tail 10 -Wait
