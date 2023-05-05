fc4ad70ec66257e613394bb87c539c91e86a072c
Cthulhux <>
2023-05-05 17:25:23 +0000

    Emacs: elpaca 0.4
    FossilOrigin-Name: 30ecb062c081b32bc5b6ed491c8626c7b6f651ed5748843f42eec4a68d108c39

--- emacs/init.el
+++ emacs/init.el
@@ -49,7 +49,7 @@
 ;; Use elpaca instead of Emacs's default package.el for
 ;; managing installed packages. This might or might not be a
 ;; good idea.
-(defvar elpaca-installer-version 0.3)
+(defvar elpaca-installer-version 0.4)
 (defvar elpaca-directory (expand-file-name "elpaca/" user-emacs-directory))
 (defvar elpaca-builds-directory (expand-file-name "builds/" elpaca-directory))
 (defvar elpaca-repos-directory (expand-file-name "repos/" elpaca-directory))
@@ -64,6 +64,7 @@
   (add-to-list 'load-path (if (file-exists-p build) build repo))
   (unless (file-exists-p repo)
     (make-directory repo t)
+    (when (< emacs-major-version 28) (require 'subr-x))
     (condition-case-unless-debug err
         (if-let ((buffer (pop-to-buffer-same-window "*elpaca-bootstrap*"))
                  ((zerop (call-process "git" nil buffer t "clone"
