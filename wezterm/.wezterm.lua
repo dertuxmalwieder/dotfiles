d6d6bef0d315d6f159e9010db2e3f0a94cfe637e
Cthulhux <>
2023-09-10 18:43:25 +0000

    WezTerm: cleanup for use on non-Windows and with config builder
    FossilOrigin-Name: dfed645c5c6f55da69095081de202cd0afb5b66d9423135d05cec4183585fac9

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,11 +1,32 @@
 local wezterm = require 'wezterm'
-return {
-	default_prog = { 'C:\\Program Files\\PowerShell\\7\\pwsh.exe', '--NoLogo' },
-	color_scheme = "Ashes (base16)",
-	font = wezterm.font_with_fallback {
-		'Consolas',
-		'JetBrains Mono',
-		'Fira Code',
-	},
-	font_size = 11.0,
+local config = {}
+
+if wezterm.config_builder then
+  config = wezterm.config_builder()
+end
+
+config.color_scheme = 'Atelier Cave Light (base16)'
+config.use_fancy_tab_bar = false
+config.font = wezterm.font { family = 'Hack' }
+config.font_size = 11.0
+
+config.window_frame = {
+  font = wezterm.font { family = 'Hack' },
+  font_size = 11.0,
 }
+
+config.colors = {
+  tab_bar = {
+    background = '#999999',
+    active_tab = {
+      bg_color = '#ababab',
+      fg_color = '#000000',
+    },
+    new_tab = {
+      bg_color = '#aaaaaa',
+      fg_color = '#000000',
+    },
+  },
+}
+
+return config
