f28d4f225bf2b84ab5f894d5b3ce63a7ed444af6
Cthulhux <>
2023-10-06 18:18:58 +0000

    Emacs: Whiskybase script: Supports search now, fixes a conversion error.
    FossilOrigin-Name: 5cfdf52415dab5934cded7f63b0f7c3ee813aed417a43a71f1071c8c72c53752

--- emacs/utilities.el
+++ emacs/utilities.el
@@ -1,14 +1,29 @@
 ;; Various functions, written for my own amusement.
+(require 'url)
 
 (defun whiskybase-open-url ()
   "Open a Whiskybase URL from a number under the cursor."
   (interactive)
-  (if (numberp (number-at-point))
-      ;; We'll need to browse to the sentence (that is the Whiskybase number, as a string)
-      (browse-url (concat "https://www.whiskybase.com/whiskies/whisky/" (sentence-at-point)))
+  (let ((has-whiskybase-item nil))
+    (if (numberp (number-at-point))
+        ;; We'll need to browse to the sentence (that is the Whiskybase number, as a string)
+        (progn ()
+               (browse-url (concat "https://www.whiskybase.com/whiskies/whisky/" (number-to-string (number-at-point))))
+               (setq has-whiskybase-item t))
 
-    ;; No such number.
-    (let ((wb-number (read-number "Enter the WhiskyBase ID: ")))
-      (if (numberp wb-number)
-          (browse-url (concat "https://www.whiskybase.com/whiskies/whisky/" (number-to-string wb-number)))
-        (message (concat "Invalid number: " wb-number ". Aborting."))))))
+      ;; No such number.
+      ;; Do we have a selection? If so, use it as a Whiskybase query.
+      (if mark-active
+          (let ((selection (buffer-substring-no-properties (region-beginning) (region-end))))
+            (if (> (length selection) 0)
+                (progn ()
+                       (browse-url (concat "https://www.whiskybase.com/search?q=" (url-hexify-string selection)))
+                       (setq has-whiskybase-item t))))))
+
+
+    (when (not has-whiskybase-item)
+      ;; We haven't found a valid Whiskybase term. Ask for an ID:
+      (let ((wb-number (read-number "Enter the WhiskyBase ID: ")))
+        (if (numberp wb-number)
+            (browse-url (concat "https://www.whiskybase.com/whiskies/whisky/" (number-to-string wb-number)))
+          (message (concat "Invalid number: " wb-number ". Aborting.")))))))
