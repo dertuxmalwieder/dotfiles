11a59acfbf69136ec872cdb8d44e15f9f6f4ec1e
Cthulhux <>
2023-10-06 19:31:03 +0000

    Emacs: moved mu4e configuration to a separate file, added BBDB support
    FossilOrigin-Name: d6fc65000348f9441f925803214b2341171dcb1ca6723fd774e5b0127e6ff3b9

--- emacs/init.el
+++ emacs/init.el
@@ -165,32 +165,7 @@
   (elpaca-process-queues))
 
 ;; E-mail:
-(add-to-list 'load-path "/opt/homebrew/share/emacs/site-lisp/mu/mu4e")
-(use-package mu4e
-  :init
-  (require 'mu4e)
-  (require 'smtpmail)
-  (require 'mu4e-speedbar)
-  :config
-  (setq mu4e-mu-binary (executable-find "mu")
-        mu4e-maildir "~/.maildir"
-        mu4e-get-mail-command (concat (executable-find "mbsync") " -a")
-        mu4e-update-interval 300
-        mu4e-index-cleanup t
-        mu4e-attachment-dir "~/Downloads"
-        mu4e-org-support t
-        mu4e-change-filenames-when-moving t
-        sendmail-program (executable-find "msmtp")
-        send-mail-function 'smtpmail-send-it
-        message-sendmail-f-is-evil t
-        message-sendmail-extra-arguments '("--read-envelope-from")
-        message-send-mail-function 'message-send-mail-with-sendmail)
-  (elpaca mu4e-alert
-    (mu4e-alert-set-default-style 'notifier)  ;; requires terminal-notifier
-    (add-hook 'after-init-hook #'mu4e-alert-enable-notifications)
-    (add-hook 'after-init-hook #'mu4e-alert-enable-mode-line-display))
-
-  (load "~/.emacs.d/mu4e-addresses")) ;; keep them out of the public eye
+(load "~/.emacs.d/mu4e-config.el")
 
 ;; Multiple cursors:
 (elpaca multiple-cursors
---
+++ emacs/mu4e-config.el
@@ -0,0 +1,37 @@
+(add-to-list 'load-path "/opt/homebrew/share/emacs/site-lisp/mu/mu4e")
+(use-package mu4e
+  :init
+  (require 'mu4e)
+  (require 'smtpmail)
+  (require 'mu4e-speedbar)
+  (require 'bbdb-loaddefs)
+  :config
+  (setq mu4e-mu-binary (executable-find "mu")
+        mu4e-maildir "~/.maildir"
+        mu4e-get-mail-command (concat (executable-find "mbsync") " -a")
+        mu4e-update-interval 300
+        mu4e-index-cleanup t
+        mu4e-attachment-dir "~/Downloads"
+        mu4e-org-support t
+        mu4e-change-filenames-when-moving t
+        sendmail-program (executable-find "msmtp")
+        send-mail-function 'smtpmail-send-it
+        message-sendmail-f-is-evil t
+        message-sendmail-extra-arguments '("--read-envelope-from")
+        message-send-mail-function 'message-send-mail-with-sendmail)
+
+  ;; Integrate BBDB:
+  (setq bbdb-mail-user-agent 'mu4e-user-agent)
+  (setq mu4e-view-rendered-hook 'bbdb-mua-auto-update)
+  (setq mu4e-compose-complete-addresses nil)
+  (setq bbdb-mua-pop-up t)
+  (setq bbdb-mua-pop-up-window-size 5)
+  (setq mu4e-view-show-addresses t)
+
+  ;; Enable notifications:
+  (elpaca mu4e-alert
+    (mu4e-alert-set-default-style 'notifier)  ;; requires terminal-notifier
+    (add-hook 'after-init-hook #'mu4e-alert-enable-notifications)
+    (add-hook 'after-init-hook #'mu4e-alert-enable-mode-line-display))
+
+  (load "~/.emacs.d/mu4e-addresses")) ;; keep login data out of the public eye
